c6c620195a05f9764efb55bfff3ecda72975e8bc
andregeorge <ageorge96.ag@gmail.com>
2024-02-08 21:36:44 +0000

    add username to comment and ability to go to another users profile page from comment

--- frontend/src/components/Post/Comment.jsx
+++ frontend/src/components/Post/Comment.jsx
@@ -1,11 +1,56 @@
+// import './Comment.css';
+
+import { useEffect, useState } from "react";
+import { getUserDataByUserId } from "../../services/user";
+import { useNavigate } from "react-router-dom";
 
 
 const Comments = (props) => {
+    const [token, setToken] = useState(window.localStorage.getItem("token"))
+    const [username, setUsername] = useState()
+    const navigate = useNavigate();
+
+
+    const handleClickOnComment = async () => {
+        const data = await getUserDataByUserId(token, props.comment.user_id)
+        const user = data.user
+        console.log(user)
+        navigate(`/profilepage/${user.username}`, 
+            {state: {
+            visiting_user_id: user._id,
+            visiting_username: user.username, 
+            visiting_profile_picture: user.profile_picture, 
+            visiting_liked_posts: user.liked_posts}})
+
+    }
+
+    useEffect(() => {
+        if (token) {
+            getUserDataByUserId(token, props.comment.user_id)
+              .then((data) => {
+                setUsername(data.user.username)
+              })
+        }
+    })
+
     return (
         <>
     <br/>
-    <article key={props.comment._id}>{props.comment.message}</article>
-    <h6><div>{props.date}</div></h6>
+        <div className='boxComment'>
+            <div className='commentSpace'>
+                {/* {check css for spantext} */}
+                <img style={{height: "40px"}} src="src/assets/profile.png" onClick={handleClickOnComment}/>
+                <div className='spanText'>
+                    <span onClick={handleClickOnComment}>{username}</span>
+                </div>
+                <p className="messageComment">
+                    <span key={props.comment._id}>{props.comment.message}</span>
+                </p>
+                <p className="dateComment">
+                    <span>{props.date}</span>
+                </p>           
+            </div>
+        </div>
     </>
     );
 };
